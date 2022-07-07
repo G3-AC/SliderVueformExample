797729941be7ec1c7e5bd060c7cc771385598851
Matthew <60680573+mbest92@users.noreply.github.com>
2022-07-07 15:23:51 -0400

    set up example slider

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,39 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <vue-slider v-model="value" />
+    <div>
+    <Slider v-model="value" />
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+//import { reactive, toRefs } from 'vue'
+//import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/antd.css'
+import Slider from '@vueform/slider'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    //VueSlider
+    Slider
+  },
+  // setup() {
+  //   const data = reactive({ value: 0 })
+  //   return toRefs(data)
+  // }
+  data() {
+    return {
+      value: 20
+    }
   }
 }
 </script>
 
-<style>
+<style src="@vueform/slider/themes/default.css">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
